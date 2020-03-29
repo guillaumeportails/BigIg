eb818ad8ba04652cacae328f2ae971d53ca5ad06
Guillaume Portails <thierry@DUBAS.localdomain>
2020-03-30 00:51:33 +0200

    Overlay

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # BigIg
 
 Firefox extension to watch big-sized Instagram pics from a user's feed
-... And to learn about firefox add-ons :
+
+And to learn about firefox add-ons :
     https://github.com/mdn/webextensions-examples
     Cf tabs-tabs-tabs, commands, menu-labelled-open
 
-
-https://extensionworkshop.com/documentation/develop/temporary-installation-in-firefox/
-	about:debugging#/runtime/this-firefox
+For temporary installation in Firefox, cf :
+    https://extensionworkshop.com/documentation/develop/temporary-installation-in-firefox/
+	about:debugging#/runtime/this-firefox3u18bu5kr
--- background.js
+++ background.js
@@ -8,43 +8,55 @@ https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs
 *****/
 
 const openLabelledId = "open-bigig";
+const title = "Big Instagram";
 const iglink = RegExp("https://www.instagram.com/p/*/");
 
 // TODO create only over "https://www.instagram.com/*/""
 browser.menus.create({
   id: openLabelledId,
-  title: "Big Instagram",
+  title: title,
   contexts: ["link"]
 });
 
 browser.menus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === openLabelledId) {
 //  browser.tabs.create({url: info.linkUrl + "media/?size=l" });
-    browser.tabs.update({url: info.linkUrl + "media/?size=l" });
-    // TODO change the DOM : add an overlay
-  }
+//  browser.tabs.update({url: info.linkUrl + "media/?size=l" });
+//  let executing = browser.tabs.executeScript ({file: "content.js"})
+    let code =
+        "var ediv = document.createElement('div');"
+      + "ediv.style.cssText = 'overflow:auto;position:fixed;top:0;left:0;width:100%;height:100%;opacity:1.0;z-index:1;';"
+      + "document.body.insertBefore(ediv, document.body.firstChild);"
+      + "var img = document.createElement('img');"
+      + "img.src = '" + info.linkUrl + "media/?size=l';"
+      + "img.style.cssText = 'display:block;margin:6px auto 6px;';"
+      + "img.onclick = function() { ediv.remove(); };"
+      + "ediv.appendChild(img);"
+      + "img.scrollIntoView();";
+      let executing = browser.tabs.executeScript ({code: code}); 
+//    executing.then(onExecuted, onError);
+    }
 });
 
+//function onExecuted(result) { console.log("done"); }
+//function onError(error) { console.log(`Error: ${error}`); }
+
 function updateMenuItem(linkElement) {
-  browser.menus.update(openLabelledId, {
-    title: `Big Ig (${linkElement.href})`
-  });
+  browser.menus.update(openLabelledId, { title: `Big Ig (${linkElement.href})` });
   browser.menus.refresh();
 }
 
 browser.menus.onShown.addListener(info => {
-  console.log(info);
   /* info.linkUrl
    * info.pageUrl    : www.instagram.com/<user>/
    * info.linkText   : lengthy
    */
   if (!info.linkUrl || !iglink.test(info.linkUrl)) {
-    browser.menus.update(openLabelledId, { title: "Big Instagram" });
+    browser.menus.update(openLabelledId, { title: title });
     browser.menus.refresh();
   } else {
     let linkElement = document.createElement("a");
     linkElement.href = info.linkUrl;
-    console.log(linkElement);
     updateMenuItem(linkElement);
   }
 });
@@ -62,5 +74,4 @@ browser.menus.create({
 
 document.addEventListener("load", onLoad);
 
-
- */
+****/
